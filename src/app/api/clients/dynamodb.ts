5ca624155ab8817d382f64c2072daabdf44f3f48
Thiago Andrade Silva <thiago.adsix@gmail.com>
2025-01-02 23:39:02 -0300

    feat: enhance DynamoDB client configuration for production environment
    - Added conditional credentials for the DynamoDB client to support secure access in production.
    - Maintained local development configuration for seamless testing and development experience.

--- src/app/api/clients/dynamodb.ts
+++ src/app/api/clients/dynamodb.ts
@@ -6,6 +6,10 @@ const isDev = process.env.NODE_ENV !== 'production';
 const client = new DynamoDBClient({
   region: 'us-east-1',
   endpoint: isDev ? 'http://localhost:4566' : undefined,
+  credentials: isDev ? undefined : {
+    accessKeyId: String(process.env.AWS_ACCESS_KEY_ID),
+    secretAccessKey: String(process.env.AWS_SECRET_ACCESS_KEY),
+  },
 });
 
 export const dynamoDbClient = DynamoDBDocumentClient.from(client);
