ca3d0d9751f34fcf6eb2135784dc37270ef40c9a
Thiago Andrade Silva <thiago.adsix@gmail.com>
2025-01-18 19:00:43 -0300

    refactor: streamline session retrieval logic in getSessions function
    - Separated the API calls for study sessions and flashcard study sessions to improve readability and maintainability.
    - Enhanced error handling by specifying the type of session in the error messages for better debugging.
    - Updated the JSON parsing to explicitly define the expected type, ensuring type safety and clarity in data handling.
    - These changes contribute to a cleaner codebase and improved error reporting in the study sessions feature.

--- src/app/(private)/study-sessions/page.tsx
+++ src/app/(private)/study-sessions/page.tsx
@@ -8,33 +8,32 @@ async function getSessions(): Promise<StudySession[]> {
   const { userId } = await auth();
   const userEmail = (await (await clerkClient()).users.getUser(userId!)).emailAddresses[0].emailAddress;
 
-  const [studySessionsResponse, flashcardsStudySessionsResponse] = await Promise.all<Response>([
-    apiClient("api/study-sessions/multiple-choices", {
-      method: "GET",
-      headers: {
-        "x-user-email": userEmail,
-      },
-      cache: "no-store",
-    }),
-    apiClient("api/study-sessions/flashcards", {
-      method: "GET",
-      headers: {
-        "x-user-email": userEmail,
-      },
-      cache: "no-store",
-    }),
-  ]);
+  const studySessionsResponse = await apiClient("api/study-sessions/multiple-choices", {
+    method: "GET",
+    headers: {
+      "x-user-email": userEmail,
+    },
+    cache: "no-store",
+  });
+
+  const flashcardsStudySessionsResponse = await apiClient("api/study-sessions/flashcards", {
+    method: "GET",
+    headers: {
+      "x-user-email": userEmail,
+    },
+    cache: "no-store",
+  });
 
   if (!studySessionsResponse.ok) {
-    throw new Error(`HTTP error! status: ${studySessionsResponse.status}`);
+    throw new Error(`HTTP error! status (multiple choices): ${studySessionsResponse.status}`);
   }
 
   if (!flashcardsStudySessionsResponse.ok) {
-    throw new Error(`HTTP error! status: ${flashcardsStudySessionsResponse.status}`);
+    throw new Error(`HTTP error! status (flashcards): ${flashcardsStudySessionsResponse.status}`);
   }
 
-  const studySessions = await studySessionsResponse.json();
-  const flashcardStudySessions = await flashcardsStudySessionsResponse.json();
+  const studySessions = await studySessionsResponse.json<StudySession[]>();
+  const flashcardStudySessions = await flashcardsStudySessionsResponse.json<StudySession[]>();
 
   return [...studySessions, ...flashcardStudySessions];
 }
