a22972eda80ac5a64cf3343a3196858928f97118
Thiago Andrade Silva <thiago.adsix@gmail.com>
2025-01-18 18:34:03 -0300

    feat: implement client-side study sessions page with filtering and calendar integration
    - Introduced a new ClientStudySessionsPage component to manage study session filtering by type, date range, and search text, enhancing user interaction.
    - Updated the package dependencies by downgrading date-fns to version 3.6.0 and adding react-day-picker version 8.10.1 for improved date handling.
    - Created a Calendar component for date selection, streamlining the user interface for selecting start and end dates.
    - Refactored the StudySessionsPage to utilize the new ClientStudySessionsPage, improving code organization and maintainability.
    - Overall, these changes enhance the functionality and user experience of the study sessions feature.

---
+++ src/app/(private)/study-sessions/client-page.tsx
@@ -0,0 +1,147 @@
+"use client";
+
+import { useState } from "react";
+import { StudySessionCard } from "./study-session-card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/src/components/ui/select";
+import { Header } from "@/src/components/header";
+import { StudySession } from "./columns";
+import { Input } from "@/src/components/ui/input";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { Calendar } from "@/src/components/ui/calendar";
+import { Button } from "@/src/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/src/components/ui/popover";
+import { cn } from "@/src/lib/utils";
+import { Label } from "@/src/components/ui/label";
+import { ptBR } from "date-fns/locale";
+
+type ClientStudySessionsPageProps = {
+  studySessions: StudySession[];
+};
+
+export function ClientStudySessionsPage({
+  studySessions,
+}: ClientStudySessionsPageProps) {
+  const [studyType, setStudyType] = useState<string>();
+  const [startDate, setStartDate] = useState<Date | undefined>();
+  const [endDate, setEndDate] = useState<Date | undefined>();
+  const [searchText, setSearchText] = useState("");
+
+  const filteredSessions = studySessions.filter(
+    (session) =>
+      (!studyType || session.studyType === studyType) &&
+      (!startDate || new Date(session.createdAt) >= startDate) &&
+      (!endDate || new Date(session.createdAt) <= endDate) &&
+      (session.deck.title.includes(searchText) ||
+        session.deck.description.includes(searchText))
+  );
+
+  return (
+    <div className="px-4 sm:px-8 flex flex-col">
+      <Header title="Sessões de estudo" mobileTitle="Sessões de estudo" />
+
+      <div className="mb-4 flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="studyType">Tipo de estudo</Label>
+          <Select onValueChange={setStudyType}>
+            <SelectTrigger>
+              <SelectValue placeholder="Selecione o tipo" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="multipleChoice">Múltipla escolha</SelectItem>
+              <SelectItem value="flashcard">Flashcard</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="searchText">Buscar por título/descrição</Label>
+          <Input
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            placeholder="Digite para buscar..."
+            id="searchText"
+          />
+        </div>
+        <div className="flex flex-col sm:flex-row gap-2 sm:gap-4">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="startDate">Data inicial</Label>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  className={cn(
+                    "w-full pl-3 text-left font-normal",
+                    !startDate && "text-muted-foreground"
+                  )}
+                  id="startDate"
+                >
+                  {startDate ? (
+                    format(startDate, "PPP", { locale: ptBR })
+                  ) : (
+                    <span>Selecione a data</span>
+                  )}
+                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                  mode="single"
+                  selected={startDate}
+                  onSelect={setStartDate}
+                  initialFocus
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="endDate">Data final</Label>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  className={cn(
+                    "w-full pl-3 text-left font-normal",
+                    !endDate && "text-muted-foreground"
+                  )}
+                  id="endDate"
+                >
+                  {endDate ? (
+                    format(endDate, "PPP", { locale: ptBR })
+                  ) : (
+                    <span>Selecione a data</span>
+                  )}
+                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                  mode="single"
+                  selected={endDate}
+                  onSelect={setEndDate}
+                  initialFocus
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+        </div>
+      </div>
+
+      <div className="grid gap-4 sm:grid-cols-2">
+        {filteredSessions.map((studySession) => (
+          <StudySessionCard key={studySession.id} studySession={studySession} />
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/app/(private)/study-sessions/page.tsx
+++ src/app/(private)/study-sessions/page.tsx
@@ -1,83 +1,31 @@
-"use client";
-
-import {Header} from "@/src/components/header";
-import { StudySession } from "./columns";
 import { auth } from "@clerk/nextjs/server";
 import { clerkClient } from "@clerk/nextjs/server";
 import { apiClient } from "@/src/lib/api-client";
-import { StudySessionCard } from "./study-session-card"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/src/components/ui/select"
-import { useState } from "react";
+import { ClientStudySessionsPage } from "./client-page";
+import { StudySession } from "./columns";
 
-export default async function StudySessionsPage() {
+async function getStudySessions(): Promise<StudySession[]> {
   const { getToken, userId } = await auth();
   const userEmail = (await (await clerkClient()).users.getUser(userId!)).emailAddresses[0].emailAddress;
-  const studySessionsResponse = await apiClient<StudySession[]>('api/study-sessions/multiple-choices', {
-    method: 'GET',
+
+  const studySessionsResponse = await apiClient("api/study-sessions/multiple-choices", {
+    method: "GET",
     headers: {
-      'Authorization': `Bearer ${await getToken()}`,
-      'x-user-email': userEmail,
+      Authorization: `Bearer ${await getToken()}`,
+      "x-user-email": userEmail,
     },
-    cache: 'no-store'
+    cache: "no-store",
   });
 
   if (!studySessionsResponse.ok) {
     throw new Error(`HTTP error! status: ${studySessionsResponse.status}`);
   }
 
-  const studySessions  = await studySessionsResponse.json();
+  return studySessionsResponse.json();
+}
 
-  const [studyType, setStudyType] = useState<string>()
-  const [startDate, setStartDate] = useState<string>()
-  const [endDate, setEndDate] = useState<string>()
-  const [searchText, setSearchText] = useState("")
+export default async function StudySessionsPage() {
+  const studySessions = await getStudySessions();
 
-  const filteredSessions = studySessions.filter(session =>
-    (!studyType || session.studyType === studyType) &&
-    (!startDate || new Date(session.createdAt) >= new Date(startDate)) &&
-    (!endDate || new Date(session.createdAt) <= new Date(endDate)) &&
-    (session.deck.title.includes(searchText) || session.deck.description.includes(searchText))
-  )
-
-  return (
-    <div className="px-8">
-      <Header title="Sessões de estudo" mobileTitle="Sessões de estudo" />
-      <div className="mb-4 flex gap-4">
-        <Select onValueChange={setStudyType}>
-          <SelectTrigger>
-            <SelectValue placeholder="Tipo de estudo" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="multipleChoice">Múltipla escolha</SelectItem>
-            <SelectItem value="flashcard">Flashcard</SelectItem>
-          </SelectContent>
-        </Select>
-        <input
-          type="date"
-          onChange={e => setStartDate(e.target.value)}
-          placeholder="Data inicial"
-        />
-        <input
-          type="date"
-          onChange={e => setEndDate(e.target.value)}
-          placeholder="Data final"
-        />
-        <input
-          type="text"
-          value={searchText}
-          onChange={e => setSearchText(e.target.value)}
-          placeholder="Buscar por título/descrição"
-        />
-      </div>
-
-      <div className="grid gap-4 md:grid-cols-2">
-        {filteredSessions.map(studySession => (
-          <StudySessionCard
-            key={studySession.id}
-            studySession={studySession}
-          />
-        ))}
-      </div>
-    </div>
-  );
+  return <ClientStudySessionsPage studySessions={studySessions} />;
 }
